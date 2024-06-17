8c7034633fac972781a7c8970e3d3d71a0646179
Oyebisi Oyekan <ooyekan@academic.rrc.ca>
2024-06-17 08:40:48 -0500

    Part 2 done

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,6 @@ class Product < ApplicationRecord
   validates :title, presence: { message: "title cannot be blank" }
   validates :price, presence: { message: "price cannot be blank" }
   validates :stock_quantity, presence: { message: "Stock quantity cannot be blank" }
+
+  belongs_to :category
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'csv'
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -9,13 +10,35 @@
 #   end
 require 'faker'
 
-676.times do
-  Product.create(
-    title: Faker::Commerce.product_name,
-    description: Faker::Lorem.paragraph,
-    price: Faker::Commerce.price(range: 0.1..100.0),
-    stock_quantity: Faker::Number.between(from: 1, to: 100)
-  )
-end
+# 676.times do
+#   Product.create(
+#     title: Faker::Commerce.product_name,
+#     description: Faker::Lorem.paragraph,
+#     price: Faker::Commerce.price(range: 0.1..100.0),
+#     stock_quantity: Faker::Number.between(from: 1, to: 100)
+#   )
+# end
 
-puts "676 products created"
+#puts "676 products created"
+
+Product.destroy_all
+Category.destroy_all
+
+  csv_file = Rails.root.join('db/products.csv')
+   csv_data = File.read(csv_file)
+
+   products = CSV.parse(csv_data, headers: true, encoding: 'iso-8859-1')
+
+   products.each do |product|
+
+    # Create categories and products here.
+    category = Category.find_or_create_by(name: product['category'])
+
+    # Create the product and associate it with the category
+  category.products.create!(
+    title: product['name'],
+    price: product['price'],
+    description: product['description'],
+    stock_quantity: product['stock quantity']
+  )
+  end
